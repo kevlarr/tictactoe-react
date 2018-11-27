bebbe53470bae992e5eb38cef0ebbb6eddd2d47d
kevlarr <echo1+git@dvnl.net>
2018-11-27 13:14:27 -0500

    Get working js/html/scss build system

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Game from './components/Game';
+import './style.scss';
 
 ReactDOM.render(
     <Game />,
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,62 @@
-module.exports = {
-    entry: './src/index.tsx',
-    output: {
-        filename: 'bundle.js',
-        path: __dirname + '/dist',
-    },
+const path = require('path');
+const WebpackMd5Hash = require('webpack-md5-hash');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-    // Enable source maps for debugging webpack output
-    devtool: 'source-map',
+module.exports = (env, argv) => {
+    const production = argv.mode === 'production';
 
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.json'],
-    },
+    return {
+        entry: './src/main.tsx',
+        output: {
+            filename: production ? '[name].[chunkhash].js' : '[name].js',
+            path: path.resolve(__dirname, 'dist'),
+        },
 
-    module: {
-        rules: [
-            { test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
-            { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
+        // Enable source maps for debugging webpack output
+        devtool: 'source-map',
+
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js', '.json', '.css', '.scss'],
+        },
+
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    loader: 'awesome-typescript-loader',
+                },
+                {
+                    test: /\.js$/,
+                    loader: 'source-map-loader', enforce: 'pre',
+                },
+                {
+                    test: /\.html$/,
+                    use: [{ loader: 'html-loader', options: { minimize: true } }],
+                },
+                {
+                    test: /\.scss$/,
+                    use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+                },
+            ],
+        },
+
+        plugins: [
+            new HtmlWebPackPlugin({
+                template: './src/index.html',
+                filename: './index.html',
+            }),
+            new MiniCssExtractPlugin({
+                filename: production ? 'style.[contenthash].css' : 'style.css',
+            }),
+            new WebpackMd5Hash(),
         ],
-    },
 
-    // Assume global variables exist rather than packaging deps up with bundle,
-    // eg. `import * from 'react'` will import from `React` global variable
-    externals: {
-        'react': 'React',
-        'react-dom': 'ReactDOM',
-    },
+        // Assume global variables exist rather than packaging deps up with bundle,
+        // eg. `import * from 'react'` will import from `React` global variable
+        //externals: {
+            //'react': 'React',
+            //'react-dom': 'ReactDOM',
+        //},
+    };
 };
