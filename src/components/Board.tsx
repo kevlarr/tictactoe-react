7c4860bad0f3cd100bb107151f82a5033dde752b
kevlarr <echo1+git@dvnl.net>
2018-11-26 23:07:34 -0500

    Square state

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -7,9 +7,9 @@ class Board extends React.Component {
 
         const rows = [0, 1, 2].map(i => (
             <div className='board-row'>
-                <Square value={(i * 3) + 0} />
-                <Square value={(i * 3) + 1} />
-                <Square value={(i * 3) + 2} />
+                <Square />
+                <Square />
+                <Square />
             </div>
         ));
 
--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,14 +1,27 @@
 import * as React from 'react';
 
 interface Props {
-    value: number;
 }
 
-class Square extends React.Component<Props, {}> {
+interface State {
+    value: string | null;
+}
+
+class Square extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+
     render() {
         return (
-            <button className='square'>
-                {this.props.value}
+            <button
+                className='square'
+                onClick={() => this.setState({ value: 'X' })}
+            >
+                {this.state.value}
             </button>
         );
     }
