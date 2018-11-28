51c002988676da9ea8306bc48e79ab7b9887190c
kevlarr <echo1+git@dvnl.net>
2018-11-28 11:39:26 -0500

    Calculate winner

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -20,6 +20,7 @@ interface State {
      */
     cells: Array<Token | null>;
     currentToken: Token;
+    won: boolean;
 };
 
 class Board extends React.Component<Props, State> {
@@ -29,16 +30,50 @@ class Board extends React.Component<Props, State> {
 
         for (let i = 0; i < 9; i++) { cells[i] = null };
 
-        this.state = { cells, currentToken: Token.X };
+        this.state = { cells, won: false, currentToken: Token.X };
     }
 
     handleClick(i: number) {
-        if (this.state.cells[i]) { return; }
+        if (this.state.won || this.state.cells[i]) { return; }
 
-        const token = this.state.currentToken;
         const cells = this.state.cells.slice();
+        const token = this.state.currentToken;
+        let won: boolean = this.state.won;
+        let nextToken = token;
+
         cells[i] = token;
-        this.setState({ cells, currentToken: token === Token.X ? Token.O : Token.X });
+
+        if (this.moveWon(cells)) {
+            won = true;
+        } else {
+            nextToken = token === Token.X ? Token.O : Token.X;
+        }
+
+        this.setState({ cells, won, currentToken: nextToken });
+    }
+
+    /**
+     * Determines if the set of cells has a winner. Requires passing in the cells
+     * to allow for calling this prior to `setState` finishing.
+     *
+     * For a small 3x3 board, checking every combination on each move is probably
+     * a little cleaner (and less breakable) than figuring out which cells to check.
+     */
+    moveWon(cells: Array<Token | null>): boolean {
+        const lines = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8], // Horizontal
+            [0, 3, 6], [1, 4, 7], [2, 5, 8], // Vertical
+            [0, 4, 8], [2, 4, 6], // Diagonals
+        ];
+
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (cells[a] && cells[a] === cells[b] && cells[b] === cells[c]) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     renderSquare(i: number) {
@@ -51,6 +86,10 @@ class Board extends React.Component<Props, State> {
     }
 
     render() {
+        const msg = this.state.won ?
+            `${this.state.currentToken} won!` :
+            `Next player: ${this.state.currentToken}`;
+
         const rows = [0, 1, 2].map(i => (
             <div key={i} className='board-row'>
                 {this.renderSquare(3 * i)}
@@ -61,7 +100,7 @@ class Board extends React.Component<Props, State> {
 
         return (
             <div className='Board'>
-                <div className='prompt-message'>Next player: {this.state.currentToken}</div>
+                <div className='prompt-message'>{msg}</div>
                 {...rows}
             </div>
         );
